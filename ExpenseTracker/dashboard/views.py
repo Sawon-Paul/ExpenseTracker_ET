c88541ddf9685e8f2e1b43b744a8301e3cb22b43
Talha-Ridwan <talharidwan77697@gmail.com>
2025-04-16 21:06:27 +0600

    Changed the structure of the APIs

--- ExpenseTracker/dashboard/views.py
+++ ExpenseTracker/dashboard/views.py
@@ -1,28 +1,33 @@
-from django.shortcuts import render
-
-# Create your views here.
-# transactions/views.py
-from rest_framework import generics, permissions
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from .models import Transactions
 from .serializers import TransactionSerializer
 
-class TransactionBaseView(generics.ListCreateAPIView):
-    serializer_class = TransactionSerializer
-    permission_classes = [permissions.IsAuthenticated]
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def transaction_list(request):
+    # Get the 'type' query parameter (optional)
+    transaction_type = request.query_params.get('type', None) #Here type is a param that is passed in the request
 
-    def get_queryset(self):
-        return Transactions.objects.filter(user=self.request.user)
+    if request.method == "GET":
+        # If 'type' is provided, filter by the specified type (cash_in or cash_out)
+        if transaction_type:
+            transactions = Transactions.objects.filter(user=request.user, type=transaction_type)
+        else:
+            # If no 'type' is specified, return all transactions for the user
+            transactions = Transactions.objects.filter(user=request.user)
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-
-class AllTransactionsView(TransactionBaseView):
-    pass
-
-class CashInTransactionsView(TransactionBaseView):
-    def get_queryset(self):
-        return super().get_queryset().filter(type='cash_in')
-
-class CashOutTransactionsView(TransactionBaseView):
-    def get_queryset(self):
-        return super().get_queryset().filter(type='cash_out')
+        # Serialize the filtered transactions
+        serializer = TransactionSerializer(transactions, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == "POST":
+        # Create a new transaction
+        serializer = TransactionSerializer(data=request.data)
+        if serializer.is_valid():
+            # Associate the transaction with the logged-in user
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
