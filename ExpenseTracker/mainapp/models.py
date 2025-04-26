224274025bab62f21836a9b6d98abfb5fa53eed4
AnamikaIslam52 <anamika.islam@g.bracu.ac.bd>
2025-04-26 21:01:18 +0600

    Update models.py

--- ExpenseTracker/mainapp/models.py
+++ ExpenseTracker/mainapp/models.py
@@ -62,3 +62,13 @@ class Feedback(models.Model):
 
     def __str__(self):
         return f"Feedback from {self.email} - {self.created_at}"
+from django.db import models
+
+class Donation(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    donated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - ${self.amount}"
